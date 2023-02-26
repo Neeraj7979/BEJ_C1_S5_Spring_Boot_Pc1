3fa3ba077f5fc9e58248971a4f046d2c1a19922c
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 00:39:25 +0530

    Created getter and setters for fields for the class movie inside domain packages

--- src/main/java/com/niit/bej/springbootc5pc1/domain/Movie.java
+++ src/main/java/com/niit/bej/springbootc5pc1/domain/Movie.java
@@ -10,4 +10,46 @@ public class Movie {
     private String movieName;
     private int yearOfRelease;
     private String genre;
+
+    public Movie(int movieID, String movieName, int yearOfRelease, String genre) {
+        this.movieID = movieID;
+        this.movieName = movieName;
+        this.yearOfRelease = yearOfRelease;
+        this.genre = genre;
+    }
+
+    public Movie() {
+    }
+
+    public int getMovieID() {
+        return movieID;
+    }
+
+    public void setMovieID(int movieID) {
+        this.movieID = movieID;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
