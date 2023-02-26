b33d24f93cfb2f1f1c7a001e021237dd7a198b33
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 00:38:05 +0530

    Created fields for the class movie inside domain packages

--- src/main/java/com/niit/bej/springbootc5pc1/domain/Movie.java
+++ src/main/java/com/niit/bej/springbootc5pc1/domain/Movie.java
@@ -1,4 +1,13 @@
 package com.niit.bej.springbootc5pc1.domain;
 
+import jakarta.persistence.Entity;
+import org.springframework.data.annotation.Id;
+
+@Entity
 public class Movie {
+    @Id
+    private int movieID;
+    private String movieName;
+    private int yearOfRelease;
+    private String genre;
 }
