3a3fee2866b51fc4efcb28a9c513c8f236163f38
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 01:03:35 +0530

    Created MovieServiceImpl class which will implement MovieService interface

---
+++ src/main/java/com/niit/bej/springbootc5pc1/service/MovieServiceImpl.java
@@ -0,0 +1,27 @@
+package com.niit.bej.springbootc5pc1.service;
+
+import com.niit.bej.springbootc5pc1.domain.Movie;
+
+import java.util.List;
+
+public class MovieServiceImpl implements MovieService {
+    @Override
+    public List<Movie> getAllMovies() {
+        return null;
+    }
+
+    @Override
+    public boolean addMovie(Movie movie) {
+        return false;
+    }
+
+    @Override
+    public boolean updateMovie(int movieId, Movie movie) {
+        return false;
+    }
+
+    @Override
+    public Movie deleteMovieById(int movieId) {
+        return null;
+    }
+}
