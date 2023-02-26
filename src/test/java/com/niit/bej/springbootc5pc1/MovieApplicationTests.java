a125dc72b9da9cd622ee29da82c09ebd89046451
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 00:21:41 +0530

    initial commit

---
+++ src/test/java/com/niit/bej/springbootc5pc1/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.bej.springbootc5pc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
