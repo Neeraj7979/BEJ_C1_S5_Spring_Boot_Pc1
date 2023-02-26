814370a426d4c5a4b61b6249859a2bc27f7d35b5
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 01:07:53 +0530

    Added dependency MovieRepository to the class MovieServiceImpl

--- src/main/java/com/niit/bej/springbootc5pc1/service/MovieServiceImpl.java
+++ src/main/java/com/niit/bej/springbootc5pc1/service/MovieServiceImpl.java
@@ -1,10 +1,19 @@
 package com.niit.bej.springbootc5pc1.service;
 
 import com.niit.bej.springbootc5pc1.domain.Movie;
+import com.niit.bej.springbootc5pc1.repositry.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class MovieServiceImpl implements MovieService {
+    private final MovieRepository movieRepository;
+
+    @Autowired
+    public MovieServiceImpl(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
     @Override
     public List<Movie> getAllMovies() {
         return null;
